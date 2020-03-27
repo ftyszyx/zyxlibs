757c2d36dbfcf4a9e8d69626fd8bf822d8313dfe
zyx <whyzi@qq.com>
2020-03-27 09:52:31 +0800

    zyx

--- string/md5.go
+++ string/md5.go
@@ -31,6 +31,16 @@ func GetByteMD5(src []byte) string {
 	return md5str1
 }
 
+func UniqueId() string {
+	b := make([]byte, 48)
+
+	if _, err := io.ReadFull(rand.Reader, b); err != nil {
+		return ""
+	}
+	return GetStrMD5(base64.URLEncoding.EncodeToString(b))
+}
+
+
 //每个字节可以存两个16进制的数字
 func KDNGetByteMD5(src []byte) string {
 	cipherStr := md5.Sum(src)
